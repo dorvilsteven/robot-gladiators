344b61a4e86da9ac65d836f9a4321b108ce441d9
StevenD <dorvilsteven@gmail.com>
2021-08-24 19:32:46 -0400

    add randomness to attacks

--- assets/js/game.js
+++ assets/js/game.js
@@ -40,7 +40,8 @@ var fight = function(enemyName) {
       }
     }
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = Math.max(0, (enemyHealth - playerAttack));
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+    enemyHealth = Math.max(0, (enemyHealth - damage));
     console.log(
       `${playerName} attacked ${enemyName}.`
     );
@@ -53,7 +54,8 @@ var fight = function(enemyName) {
       window.alert(`${enemyName} still has ${enemyHealth} health left.`);
     } 
     // remove player's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = Math.max(0, (playerHealth - enemyAttack));
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, (playerHealth - damage));
     console.log(
       `${enemyName} attacked ${playerName}.`
     );
